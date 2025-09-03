eda624c9fe116bc859f2671826e116dc56d87a81
varshitha v <varshitha.vraj@winds.co.in>
2025-09-03 12:21:57 +0530

    toggelr

--- Memorise/Memorise/ContentView.swift
+++ Memorise/Memorise/ContentView.swift
@@ -12,11 +12,50 @@ struct ContentView: View {
     
     var body: some View {
         HStack(content: {
-            Text("👀")
+            
+            CardView(isFaceUP: true)
+            CardView()
+            CardView()
+            CardView()
+            
         })
     }
     
 }
 #Preview {
     ContentView()
+}
+
+
+struct CardView: View{
+  
+    @State var isFaceUP: Bool = false
+    
+    var body: some View {
+        
+        ZStack{
+            
+//            let base = RoundedRectangle(cornerRadius: 10)
+            let base = Circle()
+            
+            if isFaceUP{
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                
+                Text("👀")
+                .font(.largeTitle)
+                
+            }else{
+                base.fill()
+                .foregroundColor(.orange)
+                base.strokeBorder(lineWidth: 2)
+                
+            }
+        }
+        .onTapGesture {
+            print("the interface is tapped")
+            isFaceUP.toggle()
+        }
+    }
+    
 }
